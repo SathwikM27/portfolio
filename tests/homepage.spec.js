7b4a445e51d8bb94c98b55563a10f62b56077f84
SathwikM <sathwikdev27@gmail.com>
2023-11-01 23:34:13 -0400

    added test to check title

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -5,3 +5,9 @@ test('check that UTF-8 tag is present', async ({ page }) => {
     const metaCharset = await page.$eval('meta[charset]', (meta) => meta.getAttribute('charset'));
     expect(metaCharset).toBe('utf-8');
 });
+
+test('check that page title is correct', async ({ page }) => {
+    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    const title = await page.title();
+    expect(title).toBe('My Portfolio');
+});
