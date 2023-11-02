56902bcd66e7739f644e1f6b994c621b58d0c059
SathwikM <sathwikdev27@gmail.com>
2023-11-01 23:07:03 -0400

    updated url in test file

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('check that UTF-8 tag is present', async ({ page }) => {
-    await page.goto('/');
+    await page.goto('http://127.0.0.1:5500/docs/index.htm');
     const metaCharset = await page.$eval('meta[charset]', (meta) => meta.getAttribute('charset'));
-    expect(metaCharset).toBe('UTF-8');
+    expect(metaCharset).toBe('utf-8');
 });
