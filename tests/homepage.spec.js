bbe85ee16bbe153fff5a2811007c274aa1583c5d
SathwikM <sathwikdev27@gmail.com>
2023-11-06 21:07:52 -0500

    started local dev server closes #57

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,73 +1,73 @@
 const { test, expect } = require('@playwright/test');
 
 test('check that UTF-8 tag is present', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const metaCharset = await page.$eval('meta[charset]', (meta) => meta.getAttribute('charset'));
     expect(metaCharset).toBe('utf-8');
 });
 
 test('check that page title is correct', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const title = await page.title();
     expect(title).toBe('My Portfolio');
 });
 
 test('check that the page has a valid doctype', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const doctype = await page.$eval('html', (html) => html.doctype.name);
     expect(doctype).toBe('html');
 });
 
 test('check that the page has a valid lang attribute', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const lang = await page.$eval('html', (html) => html.lang);
     expect(lang).toBe('en');
 });
 
 test('check that the page has a valid meta description', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const metaDescription = await page.$eval('meta[name="description"]', (meta) => meta.getAttribute('content'));
     expect(metaDescription).toBeTruthy();
 });
 
 test('check that the page has a valid meta keywords', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const metaKeywords = await page.$eval('meta[name="keywords"]', (meta) => meta.getAttribute('content'));
     expect(metaKeywords).toBeTruthy();
 });
 
 test('check that the page has a valid favicon', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const favicon = await page.$eval('link[rel="icon"]', (link) => link.getAttribute('href'));
     expect(favicon).toBeTruthy();
 });
 
 test('check that the page has a valid viewport', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const viewport = await page.$eval('meta[name="viewport"]', (meta) => meta.getAttribute('content'));
     expect(viewport).toBeTruthy();
 });
 
 test('check that the page has a valid header', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const header = await page.$('header');
     expect(header).toBeTruthy();
 });
 
 test('check that the page has a valid footer', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const footer = await page.$('footer');
     expect(footer).toBeTruthy();
 });
 
 test('check that the page has a valid navigation menu', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const navMenu = await page.$('nav');
     expect(navMenu).toBeTruthy();
 });
 
 test('check that the page has a valid resume button', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/docs/index.htm');
+    await page.goto('http://127.0.0.1:3000');
     const resumeButton = await page.$('button#resume');
     expect(resumeButton).toBeTruthy();
 });
